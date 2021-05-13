107be9c9dce591b90674384c2b3edeedf0242761
ErnanyGoncalves <39605915+ErnanyGoncalves@users.noreply.github.com>
2021-05-12 22:10:01 -0300

    Estrutura inicial da aplicação

---
+++ src/app/device/device-list/device-list-item/device-list-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-device-list-item',
+  templateUrl: './device-list-item.component.html',
+  styleUrls: ['./device-list-item.component.css']
+})
+export class DeviceListItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
